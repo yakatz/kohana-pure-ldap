5ffb264efaf1d4c8215946639ff996d80cbc0db0
Yehuda Katz <yehuda@ymkatz.net>
2016-08-24 13:06:21 -0400

    Fixed wrong IV size

--- classes/Model/LDAP/User.php
+++ classes/Model/LDAP/User.php
@@ -60,13 +60,13 @@ class SecureHandler
 
 	private static function create_iv($length){
 		if (function_exists('random_bytes')) {
-			return bin2hex(random_bytes($length));
+			return random_bytes($length);
 		}
 		if (function_exists('mcrypt_create_iv')) {
-			return bin2hex(mcrypt_create_iv($length, MCRYPT_DEV_URANDOM));
+			return mcrypt_create_iv($length, MCRYPT_DEV_URANDOM);
 		} 
 		if (function_exists('openssl_random_pseudo_bytes')) {
-			return bin2hex(openssl_random_pseudo_bytes($length));
+			return openssl_random_pseudo_bytes($length);
 		}
 		throw new Exception("No crypto function available");
 	}
